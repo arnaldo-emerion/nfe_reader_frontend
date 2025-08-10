6f163e5f1649f81ca2d0628c633bb0ca0b22ee29
Arnaldo C. Bezerra <arnaldotecadm@hotmail.com>
2025-08-10 20:48:40 +0100

    Add toggle for select all items

--- src/app/forms/analise/dist-vendas/dist-vendas-analitico/dist-vendas-analitico.component.ts
+++ src/app/forms/analise/dist-vendas/dist-vendas-analitico/dist-vendas-analitico.component.ts
@@ -143,6 +143,8 @@ export class DistVendasAnaliticoComponent implements OnInit {
         },
       },
     });
+
+    this.reloadChart();
   }
 
   openDialog(clientModel) {}
@@ -198,4 +200,42 @@ export class DistVendasAnaliticoComponent implements OnInit {
     dataFinal = null;
     this.loadChart();
   }
+
+  toggleText = 'Desmarcar Todos';
+  reloadChart() {
+    const chart = this.grafico;
+
+    // Override legend after chart exists
+    chart.options.plugins.legend.labels.generateLabels = (chartInstance: any) => {
+      const original = Chart.overrides.pie.plugins.legend.labels.generateLabels(chartInstance);
+      return [
+        {
+          text: this.toggleText,
+          fillStyle: '#888',
+          strokeStyle: '#888',
+          hidden: false,
+          index: -1
+        },
+        ...original
+      ];
+    };
+debugger;
+    chart.options.plugins.legend.onClick = (e: any, legendItem: any, legend: any) => {
+      if (legendItem.index === -1) {
+        const allVisible = chart.getDatasetMeta(0).data.every((arc: any) => !arc.hidden);
+        chart.getDatasetMeta(0).data.forEach((arc: any) => {
+          arc.hidden = allVisible;
+        });
+        this.toggleText = allVisible ? 'Marcar Todos' : 'Desmarcar Todos';
+        chart.update();
+      } else {
+        const arc = chart.getDatasetMeta(0).data[legendItem.index];
+        arc.hidden = !arc.hidden;
+        chart.update();
+      }
+    };
+
+    chart.update();
+  }
+
 }
